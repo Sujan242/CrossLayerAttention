fixed attention mask bug

model/CustomDynamicCache.py
@@ -53,8 +53,8 @@ class CustomDynamicCache(DynamicCache):
         # Gather previous token's cache from all other layers
         if self._seen_tokens > 1:
 
-            prev_token_key_cache = torch.stack([self.key_cache[i][:, :, -1, ] for i in range(len(self.key_cache)) if i != layer_idx], dim=2)
-            prev_token_value_cache = torch.stack([self.value_cache[i][:, :, -1, ] for i in range(len(self.key_cache)) if i != layer_idx], dim=2)
+            prev_token_key_cache = torch.stack([self.key_cache[i][:, :, self._seen_tokens-2,: ] for i in range(len(self.key_cache)) if i != layer_idx], dim=2)
+            prev_token_value_cache = torch.stack([self.value_cache[i][:, :, self._seen_tokens-2, ] for i in range(len(self.key_cache)) if i != layer_idx], dim=2)
 
             concatenated_keys = torch.cat([prev_token_key_cache,self.key_cache[layer_idx] ], dim=2)
             concatenated_values = torch.cat([prev_token_value_cache,self.value_cache[layer_idx]], dim=-2)

model/SmallScaleLlama.py
@@ -44,8 +44,10 @@ class CustomLlama(LlamaPreTrainedModel):
 
             current_mask = None
             # add (config.num_hidden_layers -1 ) 1s to the begginning of the attention mask
-            if attention_mask is not None:
-                current_mask = torch.cat([torch.ones(batch_size, self.config.num_hidden_layers - 1).to(device=device), attention_mask[:, :t + 1]], dim=1)
+            if t != 0:
+                current_mask = torch.cat([torch.ones(batch_size, (self.config.num_hidden_layers - 1)).to(device=device), attention_mask[:, :t + 1]], dim=1)
+            else:
+                current_mask = attention_mask[:, :t + 1]
 
             # current_position_ids = position_ids[:, t].unsqueeze(1) if position_ids is not None else None
 